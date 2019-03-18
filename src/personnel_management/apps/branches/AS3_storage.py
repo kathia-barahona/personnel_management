df67b79729832ffb481f4cc1908ab99d200d8715
Kathia Barahona <katbara03@gmail.com>
2019-03-17 19:58:59 -0600

    as3 testconfig

--- src/personnel_management/apps/branches/AS3_storage.py
+++ src/personnel_management/apps/branches/AS3_storage.py
@@ -1,6 +1,6 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class FacadeImageStorage(S3Boto3Storage):
-    location = 'facade_images'
+    location = 'media'
     default_acl = 'public-read'
     file_overwrite = False
--- src/personnel_management/settings/base.py
+++ src/personnel_management/settings/base.py
@@ -132,23 +132,43 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = os.getenv('STATIC_ROOT',
-    os.path.join(BASE_DIR, 'collected_static'))
+#USE_S3 = os.getenv('USE_S3') == 'TRUE'
 
-STATIC_URL = '/static/'
+USE_S3 = True
+
+if USE_S3:
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID','AKIAJQYAOG5GJN2C53IA')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY','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')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME','personnel-management-static')
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    AWS_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.getenv('STATIC_ROOT',
+    os.path.join(BASE_DIR, 'collected_static'))
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'compressor.finders.CompressorFinder'
 )
- 
+MEDIA_URL = '/mediafiles/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 
 
